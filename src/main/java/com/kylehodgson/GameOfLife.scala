e19ae21ddbc8e71be749d2356e5b2f6955d22ce9
Kyle Hodgson <kyle.hodgson@gmail.com>
2015-09-04 17:59:25 -0400

    fixing a bug in main

--- src/main/java/com/kylehodgson/GameOfLife.scala
+++ src/main/java/com/kylehodgson/GameOfLife.scala
@@ -5,12 +5,13 @@ object GameOfLife {
   def main(args: Array[String]) {
     println("Game of Life")
 
-    val board = new Board(5)
+    val board = new Board(20)
     println(board)
-    val iterations: Int = if(args(0).toInt > 0) args(0).toInt else 10
+    val iterations: Int = if(args.nonEmpty && args(0).toInt > 0) args(0).toInt else 100
     (1 to iterations).foreach(x => {
       board.Next
       println(board)
+      Thread.sleep(500)
     })
   }
 
